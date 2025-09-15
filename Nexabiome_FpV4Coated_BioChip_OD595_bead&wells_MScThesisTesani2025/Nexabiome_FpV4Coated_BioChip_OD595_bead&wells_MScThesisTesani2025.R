f8afbaaf7da2e70e9820d12da013115a43150439
Jasmin Tesani <144290565+jasmintesani@users.noreply.github.com>
2025-09-15 15:49:13 +0200

    Update Nexabiome_FpV4Coated_BioChip_OD595_bead&wells_MScThesisTesani2025.R

--- Nexabiome_FpV4Coated_BioChip_OD595_bead&wells_MScThesisTesani2025/Nexabiome_FpV4Coated_BioChip_OD595_bead&wells_MScThesisTesani2025.R
+++ Nexabiome_FpV4Coated_BioChip_OD595_bead&wells_MScThesisTesani2025/Nexabiome_FpV4Coated_BioChip_OD595_bead&wells_MScThesisTesani2025.R
@@ -17,7 +17,7 @@ split_name <- function(string) {
 
 data <-
   read.csv(
-    'C:\\Users/Filippo/Desktop/October exp/Evo/Nexabiome_FpV4Coated_BioChip_OD595_bead&wells_MScThesisTesani2025/Nexabiome_FpV4Coated_BioChip_OD595_bead&wells_MScThesisTesani2025.csv'
+    'C:\\Users/jasmi/Desktop/October exp/Evo/Nexabiome_FpV4Coated_BioChip_OD595_bead&wells_MScThesisTesani2025/Nexabiome_FpV4Coated_BioChip_OD595_bead&wells_MScThesisTesani2025.csv'
   )
 
 # filter data for the rest of the code
@@ -219,7 +219,7 @@ star_data <-
 # 
 # write_xlsx(summary_data,
 #            path = glue(
-#              "C:\\Users/Filippo/Desktop/October exp/Evo",
+#              "C:\\Users/jasmi/Desktop/October exp/Evo",
 #              "/Nexabiome_FpV4Coated_BioChip_OD595_bead&wells_MScThesisTesani2025",
 #              "/Nexabiome_FpV4Coated_BioChip_OD595_bead&wells_MScThesisTesani2025.xlsx"
 #            ),
